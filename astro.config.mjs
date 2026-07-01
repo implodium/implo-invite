466d9ed70a11933f957b484a9499d4b8464d5a35
quirinecker <quirin.ecker@bajupa.com>
2026-07-01 20:33:55 +0200

    moved to node and npm instead of bun. added tailwind to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import solidJs from '@astrojs/solid-js';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
 
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
